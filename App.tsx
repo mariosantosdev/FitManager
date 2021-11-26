5c13520f41a72b2552a8e6288d4f0376bcada105
Mario Santos <mariodev7@gmail.com>
2021-11-26 17:49:22 -0300

    feat(app.js): covert to typescript

---
+++ App.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import AppLoading from 'expo-app-loading';
+import { StatusBar } from 'expo-status-bar';
+import {
+    useFonts,
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold
+} from '@expo-google-fonts/poppins';
+import { NativeBaseProvider } from 'native-base'
+import { ThemeProvider } from 'styled-components';
+
+import theme, { nativeBaseTheme } from './src/utils/theme';
+import Routes from './src/routes';
+
+export default function App() {
+    const [fontsLoaded] = useFonts({
+        Poppins_300Light,
+        Poppins_400Regular,
+        Poppins_500Medium,
+        Poppins_700Bold
+    });
+
+    if (!fontsLoaded) return <AppLoading />
+
+    return (
+        <ThemeProvider theme={theme}>
+            <NativeBaseProvider theme={nativeBaseTheme}>
+                <SafeAreaProvider>
+                    <StatusBar style="auto" />
+                    <Routes />
+                </SafeAreaProvider>
+            </NativeBaseProvider>
+        </ThemeProvider>
+    );
+}
