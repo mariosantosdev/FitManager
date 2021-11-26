823fd461529b4427de4fc1f4309b4e3db32d9c78
Mario Santos <mariodev7@gmail.com>
2021-11-26 17:49:47 -0300

    feat(theme): added colors and fonts to theme

---
+++ src/utils/theme.ts
@@ -0,0 +1,43 @@
+import { extendTheme } from 'native-base';
+
+const colors = {
+    primary: '#9C9AED',
+    variations: {
+        color01: '#D8315B',
+        color02: '#3E92CC',
+        color03: '#07D962',
+        color04: '#5D5FEF',
+        color05: '#EF5DA8',
+    },
+
+    text: {
+        inLight: '#1E1B18',
+        inDark: '#FAFAFA',
+    },
+    background: {
+        light: '#FFFAFF',
+        dark: '#1E1E26'
+    }
+}
+
+const fonts = {
+    light: 'Poppins_300Light',
+    regular: 'Poppins_400Regular',
+    medium: 'Poppins_500Medium',
+    bold: 'Poppins_700Bold'
+}
+
+export const nativeBaseTheme = extendTheme({
+    colors: {
+        primary: {
+            500: colors.primary
+        }
+    },
+    fonts
+})
+
+export default {
+    colors,
+    fonts,
+};
+
