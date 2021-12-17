b531a4b83fb4d1d4e4804ddf78ddd3eafefb9222
Mario Santos <mariodev7@gmail.com>
2021-12-17 20:33:03 -0300

    feat(exercise item): created component item

---
+++ src/components/ExerciseItem/index.tsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+import {
+    Text,
+    Row as NBRow,
+    Box,
+} from "native-base";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+import { DaysValue, getDayOfWeek } from "@utils/date";
+
+import {
+    Container,
+    Title,
+    Icon,
+    Row,
+    Footer,
+} from './styles';
+import theme from "@utils/theme";
+
+interface IPropsListView {
+    item: {
+        id: number;
+        title: string;
+        day_of_week: string;
+        delay_time?: string;
+        loop?: string;
+    }
+};
+
+export default function ExerciseItem(props: IPropsListView) {
+    const { id, title, day_of_week, delay_time, loop } = props.item;
+
+    const dayOfWeek = getDayOfWeek(day_of_week as DaysValue);
+
+    return (
+        <Container key={id}>
+            <NBRow alignItems='center'>
+                <Box flex={1}>
+                    <Title numberOfLines={1}>{title}</Title>
+                </Box>
+                <MaterialCommunityIcons name="chevron-right" size={32} color={theme.colors.text.inLight} />
+            </NBRow>
+            <Footer>
+                {delay_time && (
+                    <Row>
+                        <Icon name='clock-outline' />
+                        <Text>{delay_time}</Text>
+                    </Row>
+                )}
+                {loop && (
+                    <Row>
+                        <Icon name='sync' />
+                        <Text>{loop}</Text>
+                    </Row>
+                )}
+                <Row>
+                    <Icon name='calendar-clock' />
+                    <Text>{dayOfWeek}</Text>
+                </Row>
+            </Footer>
+        </Container>
+    )
+}
---
+++ src/components/ExerciseItem/styles.ts
@@ -0,0 +1,32 @@
+import styled from "styled-components/native";
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { RFValue } from "react-native-responsive-fontsize";
+
+import {
+    Row as NBRow,
+} from 'native-base';
+
+export const Container = styled.View`
+    margin-top: ${RFValue(12)}px;
+`;
+
+export const Title = styled.Text`
+    font-size: ${RFValue(32)}px;
+    font-family: ${({ theme }) => theme.fonts.medium};
+    width: 90%;
+`;
+
+export const Footer = styled(NBRow)`
+    align-items: center;
+    padding: ${RFValue(4)}px 0px;
+`;
+
+export const Icon = styled(MaterialCommunityIcons)`
+    padding-right: ${RFValue(8)}px;
+    font-size: ${RFValue(20)}px;
+`;
+
+export const Row = styled(NBRow)`
+    margin-right: ${RFValue(20)}px;
+    align-items: center;
+`;
