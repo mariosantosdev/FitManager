69df3478f0d825e6336989e7dcaf86f0fe09425d
Mario Santos <mariodev7@gmail.com>
2021-12-22 20:39:51 -0300

    feat: treated error expired token

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,16 +1,19 @@
 import React, { useContext, useEffect, useState } from 'react';
 
+import { useToast } from 'native-base';
 import { NavigationContainer } from '@react-navigation/native';
 
+import Loading from '@components/Loading';
 import { UserContext } from '@contexts/user';
-import Inner from './Inner';
-import Auth from './Auth';
-import { getStorageToken } from '@utils/asyncStorage';
 import api from '@utils/api';
 import { ISignInResponse } from '@utils/apiTypes';
-import Loading from '@components/Loading';
+import { clearStorageToken, getStorageToken } from '@utils/asyncStorage';
+
+import Auth from './Auth';
+import Inner from './Inner';
 
 export default function Routes() {
+    const toast = useToast();
     const { user } = useContext(UserContext);
     const { setUser, setHeight, setWeight } = useContext(UserContext).actions;
 
@@ -19,16 +22,28 @@ export default function Routes() {
 
     useEffect(() => {
         async function signInUsingToken(token: string) {
-            const { data } = await api.post<ISignInResponse>('/signin/token', {}, {
-                headers: {
-                    'Authorization': `Bearer ${token}`,
-                }
-            });
-            setLoading(false);
+            try {
+                api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+                const { data } = await api.post<ISignInResponse>('/signin/token', {});
+                setLoading(false);
 
-            setUser({ ...data.user, token: data.token });
-            setWeight(data.user.weight);
-            setHeight(data.user.height);
+                setUser({ ...data.user, token: data.token });
+                setWeight(data.user.weight);
+                setHeight(data.user.height);
+            } catch (error: any) {
+                if (error.response.status === 401) {
+                    await clearStorageToken();
+                }
+
+                toast.show({
+                    title: 'Erro',
+                    description: 'Ocorreu um erro ao recuperar sua sessão. Acesse sua conta novamente.',
+                    status: 'error',
+                    placement: 'bottom',
+                });
+                setLoading(false);
+                console.log(error.toJSON());
+            }
         }
 
         async function checkExistToken() {
